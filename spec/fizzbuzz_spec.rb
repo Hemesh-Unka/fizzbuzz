1a56b22132719fd3b64293bdd472c25818fa719f
Hemesh-Unka <hemeshunka@gmail.com>
2018-02-14 13:31:10 +0000

    Buzz - Second test passed

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,8 @@ describe 'fizzbuzz' do
   it 'check if divides by 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+
+  it 'check if divides by 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
 end
